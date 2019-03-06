070bbbfaabd4ba82b0fb5f336876b988d030b536
kylepadilla <kylepadilla1@gmail.com>
2019-03-05 21:37:27 -0500

    finalized game

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -3,7 +3,12 @@
 // character toggle hide/show on map var 
 
 var playerSelect = 0;
-var enemmySelect = 0;
+var enemySelect = 0;
+
+var startAP = 0;
+var deathCounter = 0;
+// Light or Dark side selector
+var side = 0;
 
 var tempPlayer = {
   HP: 0,
@@ -18,30 +23,34 @@ var tempEnemy = {
 
 // Character list
 var sky = {
-  HP: 10,
+  HP: 100,
   AP: 10
 }
 var solo = {
-  HP: 10,
+  HP: 100,
   AP: 10
 }
 var yoda = {
-  HP: 10,
+  HP: 100,
   AP: 10
 }
 var vader = {
-  HP: 10,
+  HP: 100,
   AP: 10
 }
 var boba = {
-  HP: 10,
+  HP: 100,
   AP: 10
 }
 var jaba = {
-  HP: 10,
+  HP: 100,
   AP: 10
 }
 
+
+$(document).ready(function() {
+  $(".attack").hide();
+});
 
 $(".char").click(function() {
 console.log(this.id);
@@ -52,152 +61,280 @@ playerSelect = this.id
 // Light side characters
 
   if(playerSelect == "sky") {
+    tempPlayer = sky;
     $("#light").hide();
     $("#playerPic").html("<img src= 'assets/images/Skywalker.png'>");
-    $("#playerHP").append(sky.HP);
-    $("#playerAP").append(sky.AP);
-    $("#title-character").html("Select Your Opponent")
-    
+    $("#playerHP").html(sky.HP);
+    $("#playerAP").html(sky.AP);
+    $("#title-character").html("Select Your Opponent");
+    startAP = sky.AP   
   }
   else if(playerSelect == "solo") {
+    tempPlayer = solo;
+
     $("#light").hide();
     $("#playerPic").html("<img src= 'assets/images/Solo.png'>");
-    $("#playerHP").append(solo.HP);
-    $("#playerAP").append(solo.AP);
-    $("#title-character").html("Select Your Opponent")
-    
+    $("#playerHP").html(solo.HP);
+    $("#playerAP").html(solo.AP);
+    $("#title-character").html("Select Your Opponent");
+    startAP = solo.AP;
   }
   else if(playerSelect == "yoda") {
+    tempPlayer = yoda;
     $("#light").hide();
     $("#playerPic").html("<img src= 'assets/images/Yoda.png'>");
-    $("#playerHP").append(yoda.HP);
-    $("#playerAP").append(yoda.AP);
+    $("#playerHP").html(yoda.HP);
+    $("#playerAP").html(yoda.AP);
     $("#title-character").html("Select Your Opponent")
-    
-  }
+    startAP = yoda.AP;  }
   // Dark side characters
 
   else if(playerSelect == "vader") {
+    tempPlayer = vader;
     $("#dark").hide();
     $("#playerPic").html("<img src= 'assets/images/Darth.png'>");
-    $("#playerHP").append(vader.HP);
-    $("#playerAP").append(vader.AP);
+    $("#playerHP").html(vader.HP);
+    $("#playerAP").html(vader.AP);
     $("#title-character").html("Select Your Opponent")
-    
-  }
+    startAP = vader.AP }
+
   else if(playerSelect == "boba") {
+    tempPlayer = boba;
     $("#dark").hide();
     $("#playerPic").html("<img src= 'assets/images/BobaFett.png'>");
-    $("#playerHP").append(boba.HP);
-    $("#playerAP").append(boba.AP);
+    $("#playerHP").html(boba.HP);
+    $("#playerAP").html(boba.AP);
     $("#title-character").html("Select Your Opponent")
-    
-  }
+    startAP = boba.AP;}
+
   else if(playerSelect == "jaba") {
+    tempPlayer = jaba;
     $("#dark").hide();
     $("#playerPic").html("<img src= 'assets/images/Jabada.png'>");
-    $("#playerHP").append(jaba.HP);
-    $("#playerAP").append(jaba.AP);
+    $("#playerHP").html(jaba.HP);
+    $("#playerAP").html(jaba.AP);
     $("#title-character").html("Select Your Opponent")
-    
+    startAP = jaba.AP ;  }
   }
-
-
-
 }
+);
+
+
+// Opponent select function
+
+
+
+$(".char").click(function() {
+  console.log(this.id);
+ 
+
+  if (playerSelect !== 0 && enemySelect == 0 && this.id !== playerSelect) {
+    
+    enemySelect = this.id
+
+    if(enemySelect == "sky") {
+      tempEnemy = sky;
+     
+      $("#enemyPic").html("<img src= 'assets/images/Skywalker.png'>");
+      $("#enemyHP").html(sky.HP);
+      $("#enemyAP").html(sky.AP);
+      $("#sky").css("visibility", "hidden");
+      $(".attack").show();
+    }
+    else if(enemySelect == "solo") {
+      tempEnemy = solo;
+
+      $("#enemyPic").html("<img src= 'assets/images/Solo.png'>");
+      $("#enemyHP").html(solo.HP);
+      $("#enemyAP").html(solo.AP);
+      $("#solo").css("visibility", "hidden");
+      $("#title-character").html("Select Your Opponent");
+      $(".attack").show();
+
+    }
+    else if(enemySelect == "yoda") {
+      tempEnemy = yoda;
+
+      $("#enemyPic").html("<img src= 'assets/images/Yoda.png'>");
+      $("#enemyHP").html(yoda.HP);
+      $("#enemyAP").html(yoda.AP);
+      $("#yoda").css("visibility", "hidden");
+      $("#title-character").html("Select Your Opponent")
+      $(".attack").show();
+
+    }
+    // Dark side characters
+  
+    else if(enemySelect == "vader") {
+      tempEnemy = vader;
+
+      $("#enemyPic").html("<img src= 'assets/images/Darth.png'>");
+      $("#enemyHP").html(vader.HP);
+      $("#enemyAP").html(vader.AP);
+      $("#vader").css("visibility", "hidden");
+      $("#title-character").html("Select Your Opponent")
+      $(".attack").show();
+
+    }
+    else if(enemySelect == "boba") {
+      tempEnemy = boba;
+
+      $("#enemyPic").html("<img src= 'assets/images/BobaFett.png'>");
+      $("#enemyHP").html(boba.HP);
+      $("#enemyAP").html(boba.AP);
+      $("#boba").css("visibility", "hidden");
+      $("#title-character").html("Select Your Opponent");
+      $(".attack").show();
+
+    }
+    else if(enemySelect == "jaba") {
+      tempEnemy = jaba;
+
+      $("#enemyPic").html("<img src= 'assets/images/Jabada.png'>");
+      $("#enemyHP").html(jaba.HP);
+      $("#enemyAP").html(jaba.AP);
+      $("#jaba").css("visibility", "hidden");
+      $("#title-character").html("Select Your Opponent")
+      $(".attack").show();
+
+    }
+  }
 });
 
 
+// Attack
+
+
+$(".attack").click(function() {
+
+  console.log("attack button");
+
+  if (tempPlayer.HP > 0 && tempEnemy.HP > 0){
+
+  
+    tempPlayer.HP -= tempEnemy.AP;
+    tempEnemy.HP -= tempPlayer.AP;
+    tempPlayer.AP += startAP; 
+
+    $("#enemyHP").html(tempEnemy.HP);
+    $("#playerHP").html(tempPlayer.HP);
+    $("#playerAP").html(tempPlayer.AP);
+
+    stattrack()
+  }
+});
+
+// Player Stat Track
+
+function stattrack() {
+  if(tempPlayer.HP < 1) {
+    alert("game over")
+  }
+    if(tempEnemy.HP < 1){
+      $("#enemyPic").html("");
+      $("#enemyHP").html("");
+      $("#enemyAP").html("");
+
+      enemySelect = 0;
+
+      deathCounter++;
+
+      if(deathCounter == 3) {
+        $(".map").html("Victory!");
+        $("#dark").hide();
+        $("#title-character");
+        $("#light").hide();
+      }
+
+
+      else{
+        $(".char").click(function() {
+          console.log(this.id);
+         
+        
+          if (playerSelect !== 0 && enemySelect == 0 && this.id !== playerSelect) {
+            
+            enemySelect = this.id
+        
+            if(enemySelect == "sky") {
+              tempEnemy = sky;
+             
+              $("#enemyPic").html("<img src= 'assets/images/Skywalker.png'>");
+              $("#enemyHP").html(sky.HP);
+              $("#enemyAP").html(sky.AP);
+              $("#sky").css("visibility", "hidden");
+              $(".attack").show();
+            }
+            else if(enemySelect == "solo") {
+              tempEnemy = solo;
+        
+              $("#enemyPic").html("<img src= 'assets/images/Solo.png'>");
+              $("#enemyHP").html(solo.HP);
+              $("#enemyAP").html(solo.AP);
+              $("#solo").css("visibility", "hidden");
+              $("#title-character").html("Select Your Opponent");
+              $(".attack").show();
+        
+            }
+            else if(enemySelect == "yoda") {
+              tempEnemy = yoda;
+        
+              $("#enemyPic").html("<img src= 'assets/images/Yoda.png'>");
+              $("#enemyHP").html(yoda.HP);
+              $("#enemyAP").html(yoda.AP);
+              $("#yoda").css("visibility", "hidden");
+              $("#title-character").html("Select Your Opponent")
+              $(".attack").show();
+        
+            }
+            // Dark side characters
+          
+            else if(enemySelect == "vader") {
+              tempEnemy = vader;
+        
+              $("#enemyPic").html("<img src= 'assets/images/Darth.png'>");
+              $("#enemyHP").html(vader.HP);
+              $("#enemyAP").html(vader.AP);
+              $("#vader").css("visibility", "hidden");
+              $("#title-character").html("Select Your Opponent")
+              $(".attack").show();
+        
+            }
+            else if(enemySelect == "boba") {
+              tempEnemy = boba;
+        
+              $("#enemyPic").html("<img src= 'assets/images/BobaFett.png'>");
+              $("#enemyHP").html(boba.HP);
+              $("#enemyAP").html(boba.AP);
+              $("#boba").css("visibility", "hidden");
+              $("#title-character").html("Select Your Opponent");
+              $(".attack").show();
+        
+            }
+            else if(enemySelect == "jaba") {
+              tempEnemy = jaba;
+        
+              $("#enemyPic").html("<img src= 'assets/images/Jabada.png'>");
+              $("#enemyHP").html(jaba.HP);
+              $("#enemyAP").html(jaba.AP);
+              $("#jaba").css("visibility", "hidden");
+              $("#title-character").html("Select Your Opponent")
+              $(".attack").show();
+        
+            }
+          }
+        });
+        
 
 
 
 
+      }
+    }
+    
+};
 
+$("#reset").click(function(){
+  location.reload();
 
-
-
-
-// $(".reset").click(function() {
-//   console.log(this.id);
-// });
-
-// $(document).ready(function(){
-//   $("#button-sky").click(function(){
-//   $("#skywalker").toggle(1000);
-// });
-// });
-//   $(document).ready(function(){
-//     $("#button-sky").click(function(){
-//     $("#skywalkerSelect").show(1000);
-//   });
-
-// });
-
-// // Han Solo
-//     $(document).ready(function(){
-//       $("#button-solo").click(function(){
-//           $("#solo").hide(1000);
-//         });
-//       });
-// // Yoda
-//       $(document).ready(function(){
-//         $("#button-yoda").click(function(){
-//             $("#yoda").hide(1000);
-//           });
-//         });
-
-// // Darth Vader
-//       $(document).ready(function(){
-//           $("#button-vader").click(function(){
-//               $("#vader").hide(1000);
-//             });
-//           });
-
-// // Boba Fett
-//       $(document).ready(function(){
-//             $("#button-boba").click(function(){
-//                 $("#boba").hide(1000);
-//               });
-//             });
-
-// // Jabadahut
-//               $(document).ready(function(){
-//                 $("#button-jaba").click(function(){
-//                     $("#jaba").hide(1000);
-//                   });
-//                 });
-
-//  // sets player stats 
-
-// $(document).ready(function() {
-// $("#button").on("click", function(){
-      
-
-// });
-
-// // sets the enemy player stats
-// $(document).ready(function() {
-//   $("#button").on("click", function(){
-
-//     var enemyHP = 0
-//     var enemyAP = "";
-//     // var enemySelect = true;
-
-//     //   if (enemySelect = true) {
-
-//     //   }
-
-      
-//       for (var i = 0; i < 9; i++) {
-//         var randomNum = Math.round(Math.random() * 2);
-//         enemyHP = randomNum + enemyHP;
-//         console.log(enemyHP);
-
-//       };
-
-
-//       for (var i = 0; i < 200; i++) {
-//         var randomNum = Math.round(Math.random()*2);
-//         enemyAP = randomNum + enemyAP;
-//         console.log(enemyAP);
-//       }; 
+});
